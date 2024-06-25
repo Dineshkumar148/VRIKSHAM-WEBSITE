7562abda9a57e01c4a7b9efc69a2a73bcc89f7f0
rk7892 <ranjithraina7952@gmail.com>
2024-06-25 10:04:06 +0530

    home page animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TestimonialCardComponent } from '../../components/app-shell/components/testimonial-card/testimonial-card.component';
+import { CourseCardComponentComponent } from '../../components/app-shell/components/course-card-component/course-card-component.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, TestimonialCardComponent],
+  imports: [CommonModule, RouterLink, TestimonialCardComponent, CourseCardComponentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private elementRef: ElementRef) { }
 
+  ngAfterViewInit(): void {
+    const weAreOffering = this.elementRef.nativeElement.querySelector('.we-are-offering');
+
+    window.addEventListener('scroll', () => {
+      const scrollPosition = window.scrollY;
+      const elementPosition = weAreOffering.offsetTop;
+
+      if (scrollPosition > elementPosition - 700) {
+        weAreOffering.classList.add('animate');
+      }
+    });
+  }
 }
