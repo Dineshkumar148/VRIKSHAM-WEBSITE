823046cf4e5c0a290273bf71bef470f56f7276ff
Dineshkumar148 <140305222+Dineshkumar148@users.noreply.github.com>
2024-06-25 15:48:44 +0530

    header component and card alignment fix

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -8,11 +8,12 @@ import {
 import { ServiceOfferingCardComponent } from '../../components/app-shell/components/service-offering-card/service-offering-card.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ExplorePopupComponent } from '../../components/app-shell/components/explore-popup/explore-popup.component';
+import { TopHeaderSectionComponent } from '../../top-header-section/top-header-section.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [ServiceOfferingCardComponent],
+  imports: [TopHeaderSectionComponent, ServiceOfferingCardComponent],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
